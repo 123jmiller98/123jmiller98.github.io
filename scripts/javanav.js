5f2dda769446a477c4815be90a1d2ae8c451fd74
JOSEPH MILLER <s1458277@student.mcckc.edu>
2024-01-24 21:00:24 -0600

    uplaod java back button

---
+++ scripts/javanav.js
@@ -0,0 +1,39 @@
+// JavaScript source code
+
+
+let startUrl = "JavaSubPages/index.html"; 
+let newurl;
+
+
+useSrc();
+
+function useSrc() {
+    document.getElementById("javaembed").src = startUrl;
+}
+
+function setUrl(url) {
+    localStorage.setItem('url', url);
+}
+
+function updateDiv() {
+
+    globalUrl = localStorage.getItem('url');
+    if (globalUrl == null) {
+        newurl = startUrl;
+        calledfromblank();
+    } else {
+        newurl = globalUrl;
+    }
+    
+    document.getElementById("javaembed").src = newurl;
+    document.getElementById("javaembed").innerHTML.reload;
+
+    localStorage.setItem('url', startUrl);
+}
+
+
+
+function calledfromblank() {
+    window.print();
+
+}
